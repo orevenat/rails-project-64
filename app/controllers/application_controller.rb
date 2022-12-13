67f1a0597922a3fbbdcd9063e24d380aba2e8ef3
Ruslan Knyazev <agrry@yandex.ru>
2022-12-13 21:57:55 +0500

    add devise integration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  helper_method :current_or_guest_user
+
+  def current_or_guest_user
+    current_user || guest_user
+  end
+
+  def guest_user
+    Guest.new
+  end
 end
