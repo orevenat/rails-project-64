88944b03f88add5f0d22b57fe2d4f2c3b5a5f52c
Ruslan Knyazev <agrry@yandex.ru>
2025-12-11 17:09:30 +0500

    fix

--- app/models/post_like.rb
+++ app/models/post_like.rb
@@ -1,6 +1,6 @@
 class PostLike < ApplicationRecord
   belongs_to :post
-  belongs_to :user, foreign_key: "creator_id"
+  belongs_to :user, foreign_key: "creator_id", inverse_of: :likes
 
   validates :creator_id, uniqueness: { scope: :post_id }
 end
